3429fc3ed943c00e488d8e6b4ae60e260d457e8e
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-06 22:32:11 +0100

    Add expect_success to p-e

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Payload < ApplicationRecord
-    has_many :endpoint_payloads
-    has_many :endpoints, through: :endpoint_payloads
+  has_many :endpoint_payloads
+  has_many :endpoints, through: :endpoint_payloads
 end
