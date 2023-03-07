697f4b53943ec892f26057ff58a67123f2e99052
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-07 21:27:03 +0100

    Add nested payload in endpoint

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class Endpoint < ApplicationRecord
-    has_many :endpoint_payloads
-    has_many :payloads, through: :endpoint_payloads
+  has_many :endpoint_payloads
+  has_many :payloads, through: :endpoint_payloads
+  scope :by_name, ->(name) { where('name ILIKE ?', "%#{name}%") }
+  accepts_nested_attributes_for :payloads #, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
+  
+  def self.search(params)
+    endpoints = Endpoint.all
+    endpoints = endpoints.by_name(params[:query]) if params[:query].present?
+    endpoints
+  end
 end
