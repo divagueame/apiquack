f338112f0f2f62f4abd02741b8d3da5c3f47feb9
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-14 20:39:27 +0100

    fixing payloads

--- app/controllers/payloads_controller.rb
+++ app/controllers/payloads_controller.rb
@@ -1,39 +1,45 @@
 class PayloadsController < ApplicationController
   before_action :set_payload, only: %i[ show edit update destroy ]
 
-  # GET /payloads or /payloads.json
   def index
     @payloads = Payload.all
   end
 
-  # GET /payloads/1 or /payloads/1.json
   def show
   end
 
-  # GET /payloads/new
   def new
-    @endpoints = Endpoint.search(params)
+    if(!params[:endpoint_id].nil? && !params[:endpoint_id].empty?)
+      @selected_endpoint = Endpoint.find(params[:endpoint_id])
+    end
+
+
+    
+    if @selected_endpoint then 
+      @endpoints = Endpoint.all
+    else 
+      @endpoints = Endpoint.search(params) 
+    end
     @payload = Payload.new
   end
 
-  # GET /payloads/1/edit
   def edit
   end
 
-  # POST /payloads or /payloads.json
   def create
     @payload = Payload.new(payload_params)
-
-    respond_to do |format|
-      if @payload.save
-        format.html { redirect_to payload_url(@payload), notice: "Payload was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
+    @expect_success = params[:expect_success].present?
+    @endpoint = params[:endpoint_id]
+    p params
+    # respond_to do |format|
+    #   if @payload.save
+    #     format.html { redirect_to payload_url(@payload), notice: "Payload was successfully created." }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
-  # PATCH/PUT /payloads/1 or /payloads/1.json
   def update
     respond_to do |format|
       if @payload.update(payload_params)
@@ -44,7 +50,6 @@ class PayloadsController < ApplicationController
     end
   end
 
-  # DELETE /payloads/1 or /payloads/1.json
   def destroy
     @payload.destroy
 
@@ -55,12 +60,10 @@ class PayloadsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_payload
       @payload = Payload.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def payload_params
       params.require(:payload).permit(:data)
     end
