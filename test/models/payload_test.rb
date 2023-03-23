85c282660e3bbc27374b164288030b3ca5a544aa
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-23 21:17:18 +0100

    Add tests for payload json validation

--- test/models/payload_test.rb
+++ test/models/payload_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 class PayloadTest < ActiveSupport::TestCase
-  test "data should a valid json object" do
+  test "Payload is valid if data is an empty object" do
+    payload = Payload.new(data: '{}')
+    payload.valid?
+    assert_empty payload.errors[:payload]
+  end
+
+  test "Payload is valid if data is a correct json object" do
+    payload = Payload.new(data: '{"meow":20, "kitty": true}')
+    payload.valid?
+    assert_empty payload.errors[:payload]
   end
 
   test "should not be created if data is not a valid json object" do
+    payload = Payload.new(data: '{invalid JSON}')
+    assert_equal false, payload.valid?
+    assert_not_empty payload.errors[:data]
   end
 end
