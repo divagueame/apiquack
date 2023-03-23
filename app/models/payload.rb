0e2b82fca20ffa47da8e8b707545582dd21e041d
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-23 21:17:41 +0100

    Add json validation in payload model

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -3,4 +3,15 @@
 class Payload < ApplicationRecord
   has_many :endpoint_payloads
   has_many :endpoints, through: :endpoint_payloads
+  validate :validate_json
+
+  private
+
+  def validate_json
+    begin
+      JSON.parse(data)
+    rescue JSON::ParserError => e
+      errors.add(:data, "Ooops... Not a valid json object: #{e.message}")
+    end
+  end
 end
