97427a6412605b9f7e540d87d67755c7d6b50b8c
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-26 22:50:32 +0200

    Add test + images

--- test/controllers/endpoints_controller_test.rb
+++ test/controllers/endpoints_controller_test.rb
@@ -2,26 +2,26 @@ require "test_helper"
 
 class EndpointsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @endpoint = endpoints(:one)
+    # @endpoint = Endpoint.all.first
   end
 
-  # test "should get index" do
-  #   get endpoints_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get endpoints_url
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get new_endpoint_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_endpoint_url
+    assert_response :success
+  end
 
-  # test "should create endpoint" do
-  #   assert_difference("Endpoint.count") do
-  #     post endpoints_url, params: { endpoint: { description: @endpoint.description, name: @endpoint.name, url: @endpoint.url } }
-  #   end
+  test "should create endpoint" do
+    assert_difference("Endpoint.count") do
+      post endpoints_url, params: { endpoint: { description: "Description for this endpoint", name: "New endpoint name", url: '/' } }
+    end
 
-  #   assert_redirected_to endpoint_url(Endpoint.last)
-  # end
+    assert_redirected_to endpoint_url(Endpoint.last)
+  end
 
   # test "should show endpoint" do
   #   get endpoint_url(@endpoint)
