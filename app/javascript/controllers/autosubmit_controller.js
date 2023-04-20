320eab5605618ed252779d42959e87c1810049de
Martin Arce <martin.arce.teixeira@gmail.com>
2023-04-20 21:16:48 +0200

    add stimulus controller

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -23,4 +23,10 @@ export default class extends Controller {
     this.queryFieldTarget.value = ''
     this.submit()
   }
+  triggerSelect(e){
+    const endpointId = parseInt(e.target.dataset.val)
+    const hiddeni = document.getElementById('endpoint_id')
+    hiddeni.value = endpointId
+    this.submit()
+  }
 }
